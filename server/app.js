36cb2698cfd552128edfb2b043ec5e6695600f36
Haneef <haneefshiraz1@gmail.com>
2020-09-13 22:55:56 +0530

    change code

--- server/app.js
+++ server/app.js
@@ -3,39 +3,46 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var dbSetting = require('./src/config/env.config')
+var env=require('./src/config/env.config')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var auth = require('./src/routes/auth.route');
+// var dashboard = require('./src/routes/dashboard.route');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('dbSetting', dbSetting);
+
+
+// Setup database _connection
+let SETTINGS = app.get('dbSetting');
+let db = require("./src/components/db").connect(SETTINGS);
+
+app.set('db', db);
+
+
+// app.use(function (req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', "*");
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,authorization');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   next();
+// });
+
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../fontend/client')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/apiV1/auth', auth);
+// app.use('/apiV1/dashboard', dashboard);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use('*',(req,res)=>{
+  console.log(path.join(__dirname, '../fontend/client/index.html'))
+  return res.sendFile(path.join(__dirname, '../fontend/client/index.html'))
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+})
 
 module.exports = app;
